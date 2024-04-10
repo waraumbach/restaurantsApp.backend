f1bbccc55f28f25dbc237bd4ce4c3470fc1485e7
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-10 17:07:42 +0200

    nodemon

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (request, response) => {
-  response.send("This is my API running...");
-});
-
-app.listen(8000, () => {
-  console.log(`server is running on port 8000`);
-});
+console.log("hello");
