a70cf72fb2e8342a0c63ee67c4b8a44282fb2dda
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-10 17:31:07 +0200

    updated file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("welcome to my restaurant API");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
