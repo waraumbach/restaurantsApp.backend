8640f33dc44994382c8f7eeb051dbba737d2e6c3
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-10 19:13:03 +0200

    added restaurantsData file

---
+++ data/restaurantsData.js
@@ -0,0 +1,47 @@
+const restaurantsData = [
+  {
+    id: 1,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+    image:
+      "https://thumbs.dreamstime.com/z/kfc-gastst%C3%A4tteinnenraum-64599854.jpg",
+  },
+  {
+    id: 2,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 3,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 4,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 5,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 5,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+];
+export default restaurantsData;
--- index.js
+++ index.js
@@ -1,51 +1,7 @@
 import express from "express";
+import restaurantsData from "./data/restaurantsData.js";
 
 const app = express();
-
-const restaurantsData = [
-  {
-    id: 1,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-  {
-    id: 2,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-  {
-    id: 3,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-  {
-    id: 4,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-  {
-    id: 5,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-  {
-    id: 5,
-    name: "kfc",
-    reviews: "4",
-    location: "Germany",
-    type: "fast food",
-  },
-];
 
 app.get("/", (request, response) => {
   response.send("welcome to my restaurant API");
@@ -54,7 +10,7 @@ app.get("/", (request, response) => {
 app.get("/restaurants", (request, response) => {
   response.json(restaurantsData);
 });
-
+//call port
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
