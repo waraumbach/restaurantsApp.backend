2dd1932656136a105eeedbdd1b9605ca98c1d3c8
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-10 19:01:13 +0200

    routes data on localhost

--- index.js
+++ index.js
@@ -2,8 +2,57 @@ import express from "express";
 
 const app = express();
 
+const restaurantsData = [
+  {
+    id: 1,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 2,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 3,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 4,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 5,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+  {
+    id: 5,
+    name: "kfc",
+    reviews: "4",
+    location: "Germany",
+    type: "fast food",
+  },
+];
+
 app.get("/", (request, response) => {
   response.send("welcome to my restaurant API");
+});
+//route : data// restaurantsdata to localhost 30000
+app.get("/restaurants", (request, response) => {
+  response.json(restaurantsData);
 });
 
 app.listen(3000, () => {
