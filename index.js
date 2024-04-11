b6ac92bdb274deebff9346bbc05014c80b8ffe4d
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-11 14:33:56 +0200

    updated

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.use(cors());
 app.get("/", (request, response) => {
   response.send("welcome to my restaurant API");
 });
-//route : data// restaurantsdata to localhost 30000
-app.get("/restaurants", (request, response) => {
-  response.json(restaurantsData);
+app.get("/restaurants", (req, res) => {
+  res.json(restaurantsData);
 });
 //access to data to id
-app.get("/restaurants/:id", (request, response) => {
-  let { id } = request.params;
+app.get("/restaurants/:id", (req, res) => {
+  let { id } = req.params;
   const restaurant = restaurantsData.find(
     (restaurant) => restaurant.id === parseInt(id)
   );
-  request.json(restaurant);
+  res.json(restaurant);
 });
+
 //call port
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
